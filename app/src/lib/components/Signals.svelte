b3d408f1c377f2303822ca08724fec81575cb0a3
agol <andre.goldmann@hacon.de>
2023-10-10 23:11:17 +0200

    deleting and updating order (frontend)

--- app/src/lib/components/Signals.svelte
+++ app/src/lib/components/Signals.svelte
@@ -176,81 +176,78 @@
             </div>
         </div>
 
-        <!--div>
-            <p>{ $tradeSelected }</p>
-        </div-->
+    {#if $tradeSelected}
+        <div class="basis-1/4">
+            <!--form class="flex items-center space-x-6" action="http://127.0.0.1:6081/createorder" on:submit|preventDefault={handleSubmit} method="POST"-->
+            <form class="flex items-center space-x-6" action="http://85.215.32.163:6081/modifyorder" on:submit|preventDefault={handleSubmit} method="POST">
+                <div class="bg-transparent px-6 py-8 rounded shadow-md text-primary w-full">
+                    <h1 class="mb-8 text-3xl text-center">New Order</h1>
+                    <div class="w-full max-w-xs">
+                        <!--label class="label" for="symbols">
+                            <span class="label-text">Symbol</span>
+                        </label-->
+                        <input
+                                type="text"
+                                class="input input-bordered input-primary w-full max-w-xs mb-4"
+                                id="symbol"
+                                name="symbol"
+                                value={$tradeSelected.symbol}
+                                disabled/>
+                    </div>
 
-    <div class="basis-1/4">
-        <!--form class="flex items-center space-x-6" action="http://127.0.0.1:6081/createorder" on:submit|preventDefault={handleSubmit} method="POST"-->
-        <form class="flex items-center space-x-6" action="http://85.215.32.163:6081/modifyorder" on:submit|preventDefault={handleSubmit} method="POST">
-            <div class="bg-transparent px-6 py-8 rounded shadow-md text-primary w-full">
-                <h1 class="mb-8 text-3xl text-center">New Order</h1>
-                <div class="w-full max-w-xs">
-                    <!--label class="label" for="symbols">
-                        <span class="label-text">Symbol</span>
-                    </label-->
+                    <div class="w-full max-w-xs">
+
+                        <input
+                                type="text"
+                                class="input input-bordered input-primary w-full max-w-xs mb-4"
+                                id="type"
+                                name="type"
+                                value={$tradeSelected.type}
+                                disabled/>
+                    </div>
+
                     <input
-                            type="text"
+                            type="number"
+                            step=".0001"
                             class="input input-bordered input-primary w-full max-w-xs mb-4"
-                            id="symbol"
-                            name="symbol"
-                            value={$tradeSelected.symbol}
-                            disabled/>
-                </div>
-
-                <div class="w-full max-w-xs">
+                            id="entry"
+                            name="entry"
+                            value={$tradeSelected.entry}
+                            placeholder="Entry" />
 
                     <input
-                            type="text"
+                            type="number"
+                            step=".0001"
                             class="input input-bordered input-primary w-full max-w-xs mb-4"
-                            id="type"
-                            name="type"
-                            value={$tradeSelected.type}
-                            disabled/>
+                            id="sl"
+                            name="sl"
+                            value={$tradeSelected.sl}
+                            placeholder="Stoploss" />
+
+                    <input
+                            type="number"
+                            step=".0001"
+                            class="input input-bordered input-primary w-full max-w-xs mb-4"
+                            id="tp"
+                            name="tp"
+                            value={$tradeSelected.tp}
+                            placeholder="Take Profit" />
+                    <input
+                            type="number"
+                            step=".001"
+                            class="input input-bordered input-primary w-full max-w-xs mb-4"
+                            id="lots"
+                            name="lots"
+                            value={$tradeSelected.lots}
+                            placeholder="Volume/Lots" />
+
+                    <div>
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        <button type="button" class="btn btn-primary">Delete</button>
+                    </div>
+
                 </div>
-
-                <input
-                        type="number"
-                        step=".0001"
-                        class="input input-bordered input-primary w-full max-w-xs mb-4"
-                        id="entry"
-                        name="entry"
-                        value={$tradeSelected.entry}
-                        placeholder="Entry" />
-
-                <input
-                        type="number"
-                        step=".0001"
-                        class="input input-bordered input-primary w-full max-w-xs mb-4"
-                        id="sl"
-                        name="sl"
-                        value={$tradeSelected.sl}
-                        placeholder="Stoploss" />
-
-                <input
-                        type="number"
-                        step=".0001"
-                        class="input input-bordered input-primary w-full max-w-xs mb-4"
-                        id="tp"
-                        name="tp"
-                        value={$tradeSelected.tp}
-                        placeholder="Take Profit" />
-                <input
-                        type="number"
-                        step=".001"
-                        class="input input-bordered input-primary w-full max-w-xs mb-4"
-                        id="lots"
-                        name="lots"
-                        value={$tradeSelected.lots}
-                        placeholder="Volume/Lots" />
-
-                <div>
-                    <button type="submit" class="btn btn-primary">Update</button>
-                    <button type="button" class="btn btn-primary">Delete</button>
-                </div>
-
-            </div>
-        </form>
-    </div>
-
+            </form>
+        </div>
+    {/if}
 </div>
