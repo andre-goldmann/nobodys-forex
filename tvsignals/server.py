9da9b4bc8dae237e89d8cac260674d04c6bce3f0
andre-goldmann <andre-goldmann@yahoo.de>
2023-10-22 18:08:32 +0200

    strategies added

--- tvsignals/server.py
+++ tvsignals/server.py
@@ -21,7 +21,7 @@ session = Session()
 app = FastAPI()
 
 symbols = ["AUDUSD", "AUDCHF", "AUDJPY", "AUDNZD", "CHFJPY", "EURUSD", "EURCHF", "EURNZD", "GBPUSD", "GBPCAD", "GBPCHF", "GBPNZD",  "XAGUSD", "USDCAD", "USDCHF", "XRPUSD"]
-strategies = ["NNR", "Super AI Trend", "70% Strategy"]
+strategies = ["NNR", "Super AI Trend", "70% Strategy", "SSL Hybrid", "AI Volume Supertrend", "SSL + Wave Trend Strategy"]
 
 # does not work
 #app.add_middleware(
