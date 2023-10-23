ff0dd1b7729f84b0f743921dc7ac682ecbb235f3
andre-goldmann <andre-goldmann@yahoo.de>
2023-10-23 22:17:31 +0200

    activated regression again

--- server.py
+++ server.py
@@ -554,14 +554,14 @@ if __name__ == "__main__":
                         print(f"Inserted data for {symbol} + {timeFrame}")
 
     #TODO on startup go through like this load the last candle and from this candle on load all until now other metatrade
-    #for symbol in symbols:
-    #    for timeFrame in TimeFrame:
-    #        if TimeFrame.PERIOD_H4 is timeFrame or TimeFrame.PERIOD_D1 is timeFrame or TimeFrame.PERIOD_W1 is timeFrame:
-    #            deleteSupportResistance(symbol, timeFrame)
-    #            trendlinebreakout(symbol, timeFrame)
-    #            autoDetectSupportAndResistance(symbol, 30000, 20, timeFrame)
-    #            defaultsr(symbol, 0.01, timeFrame)
-    #            regressionCalculation(symbol,"2023-01-01 00:00:00.000000", timeFrame)
+    for symbol in symbols:
+        for timeFrame in TimeFrame:
+            if TimeFrame.PERIOD_H4 is timeFrame or TimeFrame.PERIOD_D1 is timeFrame or TimeFrame.PERIOD_W1 is timeFrame:
+                deleteSupportResistance(symbol, timeFrame)
+                trendlinebreakout(symbol, timeFrame)
+                autoDetectSupportAndResistance(symbol, 30000, 20, timeFrame)
+                defaultsr(symbol, 0.01, timeFrame)
+                regressionCalculation(symbol,"2023-01-01 00:00:00.000000", timeFrame)
 
 
 
