d86ea2c9ac621d01392d5b944f1ca3f6a95ef8d9
andre-goldmann <andre-goldmann@yahoo.de>
2023-12-16 18:43:16 +0100

    convert pine script to python

--- server.py
+++ server.py
@@ -300,10 +300,14 @@ async def redkslow(symbol:str, timeframe: str):
     data['Low'] = data.LOW
     data['close'] = data.CLOSE
     data['Volume'] = data.TICKVOL
+    print("++++++++++++++++++")
     print(data['close'].tail(-1).tail(200))
     print(data['close'].iloc[len(data)-200:len(data)-1])
+    print("++++++++++++++++++")
     LL = f_LazyLine(data['close'].tail(-1).tail(200), 15)
+    print(f"{LL}")
     LLPrev = f_LazyLine(data['close'].iloc[len(data)-200:len(data)-1], 15)
+    print(f"{LLPrev}")
     #uptrend     = LL > LL[1]
     #if len(result) > 0:
     #    return {'startTime': result[0].startTime, 'endTime': result[0].endTime, 'startValue': result[0].startValue, 'endValue': result[0].endValue}
