75efa32ef5536a398bed493fb4b643cfbcc6396b
andre-goldmann <andre_goldmann@yahoo.de>
2023-12-20 22:20:25 +0100

    just 3 signals at the same time

--- server.py
+++ server.py
@@ -1142,9 +1142,11 @@ async def storeCandle(candle:CandlesDto):
             dfM15 = loadDfFromDb(symbol, TimeFrame.PERIOD_M15, 10)
             entry = dfM15.iloc[0].CLOSE
 
+        # Just always 3 to not interupt TradingView-Signals
         adx(df, symbol, timeframeEnum, entry)
-        #adxEma14(df, symbol, timeframeEnum, entry)
-        #adxRsi(df, symbol, timeframeEnum, entry)
+        adxEma14(df, symbol, timeframeEnum, entry)
+        adxRsi(df, symbol, timeframeEnum, entry)
+
         #awesomeOscillatorSaucer(df, symbol, timeframeEnum, entry)
         #awesomeOscillatorZeroCrossover(df, symbol, timeframeEnum, entry)
         #bladeRunner(df, symbol, timeframeEnum, entry)
