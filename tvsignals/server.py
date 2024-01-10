979636eb6813aabbf5143e6a36c11585476bf802
andre-goldmann <andre_goldmann@yahoo.de>
2024-01-10 11:41:40 +0100

    if trendscore is higher/lower 7/-7 store the information

--- tvsignals/server.py
+++ tvsignals/server.py
@@ -602,20 +602,22 @@ def proceedSignal(signal):
 
         trendInfo = session.query(TrendInfoEntity).filter(TrendInfoEntity.symbol == signal.symbol).first()
         if trendInfo is not None:
-            if trendInfo.trendscore > -7 and signal.type == "sell":
+            if trendInfo.trendscore >= -7 and signal.type == "sell":
                 print(f"Ignore Signal because signal {signal} is against trend: {trendInfo.trendscore} \n")
-                #storeIgnoredSignal(IgnoredSignal(
-                #    json=jsonSignal,
-                #    reason=f"Ignore Signal because signal {signal} is against trend: {trendInfo.uptrend}"
-                #),session)
+                if trendInfo.trendscore <= -7:
+                    storeIgnoredSignal(IgnoredSignal(
+                        json=jsonSignal,
+                        reason=f"Ignore Signal because signal {signal} is against trend: {trendInfo.uptrend}"
+                    ),session)
                 session.close()
                 return
-            if trendInfo.trendscore < 7 and signal.type == "buy":
+            if trendInfo.trendscore <= 7 and signal.type == "buy":
                 print(f"Ignore Signal because signal {signal} is against trend: {trendInfo.trendscore} \n")
-                #storeIgnoredSignal(IgnoredSignal(
-                #    json=jsonSignal,
-                #    reason=f"Ignore Signal because signal {signal} is against trend: {trendInfo.uptrend}"
-                #),session)
+                if trendInfo.trendscore >= 7:
+                    storeIgnoredSignal(IgnoredSignal(
+                        json=jsonSignal,
+                        reason=f"Ignore Signal because signal {signal} is against trend: {trendInfo.uptrend}"
+                    ),session)
                 session.close()
                 return
         if trendInfo is None:
