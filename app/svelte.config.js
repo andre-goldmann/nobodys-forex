ab402f644408b61b6a398771128c96e0d5c740a0
andre-goldmann <andre_goldmann@yahoo.de>
2024-03-14 15:52:55 +0100

    using nginx to route to the app

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +9,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapterMulti([
+			adapterStatic({ pages: 'build-static', assets: 'build-static', precompress: false }),
+			adapterNode({ out: 'build-node', precompress: false })
+		]),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	}
 };
 
