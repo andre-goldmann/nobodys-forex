f062108ac457c40abb1b885e48b190a9d93f239a
andre-goldmann <andre_goldmann@yahoo.de>
2024-03-26 09:26:08 +0100

    start implementing frontend end java-backend

---
+++ forex-backend-java/src/main/java/jdg/digital/forexbackend/ForexBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package jdg.digital.forexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexBackendJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForexBackendJavaApplication.class, args);
+    }
+
+}
---
+++ forex-backend-java/src/test/java/jdg/digital/forexbackend/ForexBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package jdg.digital.forexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexBackendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
