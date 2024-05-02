4c8cf954a6b8ec293638a6d8823d690740a065ef
agol <andre.goldmann@hacon.de>
2024-05-02 09:56:47 +0200

    test remote

--- angular-projects/apps/forex-app/src/environments/environment.ts
+++ angular-projects/apps/forex-app/src/environments/environment.ts
@@ -2,11 +2,11 @@ import { AppConfig } from '@angular-projects/app-config';
 
 export const environment: AppConfig = {
   production: false,
-  baseURL: 'https://85.215.32.163:9080/api/forex',
+  baseURL: 'https://85.215.32.163/api/forex',
   clientId: 'forex_admininstrator_client',
   clientSecret: 'L7YFcYYKDDpIG6dvBtISskp40O8RiyXd',
   webHost: window.location.origin,
-  keycloakHost: 'https://85.215.32.163:8443/realms/forex_admininstrator',
+  keycloakHost: 'https://85.215.32.163/realms/forex_admininstrator',
   requireHttps: false
 };
 //TODO download keycloak to have no problems when offline
