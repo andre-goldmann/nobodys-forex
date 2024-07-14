909ebae42be564dab0ace848695d46961e82f048
andre-goldmann <andre_goldmann@yahoo.de>
2024-07-14 14:41:12 +0200

    Added a new trading strategy, SmaCross, which uses the crossover of two simple moving averages to generate buy and sell signals.

--- hello.py
+++ hello.py
@@ -1,9 +1,26 @@
 from sqlalchemy import create_engine
 import pandas as pd
-from backtesting import Backtest
+from backtesting import Backtest, Strategy
 from trading_strategies.adx_crossover import AdxCrossover
 from trading_strategies.aroon_adx import AroonAdx
+from backtesting.lib import crossover
+from bokeh.models import DatetimeTickFormatter
+from backtesting.test import SMA
 
+class SmaCross(Strategy):
+    n1 = 10
+    n2 = 20
+
+    def init(self):
+        close = self.data.Close
+        self.sma1 = self.I(SMA, close, self.n1)
+        self.sma2 = self.I(SMA, close, self.n2)
+
+    def next(self):
+        if crossover(self.sma1, self.sma2):
+            self.buy()
+        elif crossover(self.sma2, self.sma1):
+            self.sell()
 
 def main():
     # Database connection details
@@ -37,9 +54,9 @@ def main():
         'TICKVOL': 'Volume'
     })
     df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
-
+    print(df.tail())
     # Run backtest
-    bt = Backtest(df, AdxCrossover, cash=10000, commission=.002)
+    bt = Backtest(df, SmaCross, cash=10000, commission=.002)
     #bt = Backtest(df, AroonAdx, cash=10000, commission=.002)
 
     results = bt.run()
