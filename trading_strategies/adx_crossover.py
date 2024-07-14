20db9daad65d1c9dcb400ec433bf2c1486c4b3d3
andre-goldmann <andre_goldmann@yahoo.de>
2024-07-14 20:35:53 +0200

    reset strategy

--- trading_strategies/adx_crossover.py
+++ trading_strategies/adx_crossover.py
@@ -5,64 +5,100 @@ Created on Sun Sep 27 09:29:36 2020
 @author: mingyu
 """
 
-import numpy as np
-from backtesting import Strategy
-from backtesting.lib import crossover
+import pandas as pd
+import ta
+#import talib as ta
+import trading_strategies.visualise as v
 
-class AdxCrossover(Strategy):
-    window = 20
-    adx_threshold = 25
+class AdxCrossover:
+    #def __init__(self, file_path):
+    def __init__(self, df):
+        #self.df = pd.read_csv(file_path)
+        self.df = df#pd.DataFrame(file_path, columns=("time", "open", "high", "low", "close", "tick_volume","pos"))
+        self.high = self.df['high']
+        self.low = self.df['low']
+        self.close = self.df['close']
 
-    def init(self):
-        close = self.data.Close
-        high = self.data.High
-        low = self.data.Low
+    def calculate_adx(self):
+        self.df['adx'] = ta.trend.ADXIndicator(high = self.high, low = self.low, close = self.close, window = 20).adx()
 
-        self.tr = self.I(lambda: np.maximum(
-            high - low,
-            np.abs(high - np.roll(close, 1)),
-            np.abs(low - np.roll(close, 1))
-        ), name='TR')
-        
-        self.plus_dm = self.I(lambda: np.where(
-            (high - np.roll(high, 1) > np.roll(low, 1) - low) &
-            (high - np.roll(high, 1) > 0),
-            high - np.roll(high, 1),
-            0
-        ), name='Plus_DM')
-        
-        self.minus_dm = self.I(lambda: np.where(
-            (np.roll(low, 1) - low > high - np.roll(high, 1)) &
-            (np.roll(low, 1) - low > 0),
-            np.roll(low, 1) - low,
-            0
-        ), name='Minus_DM')
-        
-        self.smoothed_tr = self.I(lambda: self.smoothed(self.tr, self.window), name='Smoothed_TR')
-        self.smoothed_plus_dm = self.I(lambda: self.smoothed(self.plus_dm, self.window), name='Smoothed_Plus_DM')
-        self.smoothed_minus_dm = self.I(lambda: self.smoothed(self.minus_dm, self.window), name='Smoothed_Minus_DM')
-        
-        self.plus_di = self.I(lambda: np.where(self.smoothed_tr != 0, 100 * self.smoothed_plus_dm / self.smoothed_tr, 0), name='Plus_DI')
-        self.minus_di = self.I(lambda: np.where(self.smoothed_tr != 0, 100 * self.smoothed_minus_dm / self.smoothed_tr, 0), name='Minus_DI')
-        self.dx = self.I(lambda: np.where((self.plus_di + self.minus_di) != 0, 100 * np.abs(self.plus_di - self.minus_di) / (self.plus_di + self.minus_di), 0), name='DX')
-        self.adx = self.I(lambda: self.smoothed(self.dx, self.window), name='ADX')
+    def calculate_minus_DI(self):
+        self.df['-di'] = ta.trend.ADXIndicator(high = self.high, low = self.low, close = self.close, window = 20).adx_neg()
 
-    def next(self):
-        if self.adx[-1] > self.adx_threshold:
-            if crossover(self.plus_di, self.minus_di):
-                self.buy()
-            elif crossover(self.minus_di, self.plus_di):
-                self.sell()
 
-        if self.position.is_long and crossover(self.minus_di, self.plus_di):
-            self.position.close()
-        elif self.position.is_short and crossover(self.plus_di, self.minus_di):
-            self.position.close()
+    def calculate_plus_DI(self):
+        self.df['+di'] = ta.trend.ADXIndicator(high = self.high, low = self.low, close = self.close, window = 20).adx_pos()
 
-    def smoothed(self, series, window):
-        alpha = 1 / window
-        def smooth(x):
-            if len(x) < 2:
-                return x
-            return x[0] if np.isnan(x[1]) else alpha * x[0] + (1 - alpha) * x[1]
-        return self.I(smooth, series, name=f'EMA_{window}')
+
+    def determine_signal(self, dframe):
+
+        # initialise signal to hold: 0
+        signal = 0
+
+        # BUY SIGNAL: adx is above 25 and the positive DI crosses over negative DI indicates a strong uptrend
+        if dframe['adx'].iloc[-1] > 25 and dframe['+di'].iloc[-1] > dframe['-di'].iloc[-1] and dframe['+di'].iloc[-2] <= dframe['-di'].iloc[-2]:
+            signal = 1
+        # SELL SIGNAL: adx is above 25 and the negative DI crosses over positive DI indicates a strong downtrend
+        elif dframe['adx'].iloc[-1] > 25 and dframe['+di'].iloc[-1] < dframe['-di'].iloc[-1] and dframe['+di'].iloc[-2] >= dframe['-di'].iloc[-2]:
+            signal = -1
+
+        return (signal, dframe['close'].iloc[-1])
+
+    def run(self):
+        self.calculate_adx()
+        self.calculate_minus_DI()
+        self.calculate_plus_DI()
+        signal = self.determine_signal(self.df)
+        return signal, self.df
+
+    ''' The following methods are for plotting '''
+
+    def find_all_signals(self, plot_df):
+        # assign intitial value of hold
+        plot_df['signal'] = 0
+
+        start = -1 * len(
+            plot_df)  # using negative indices just in case you are using a subset of input data where index does not start at index 0
+        end = start + 40  # where the current window will stop (exclusive of the element at this index)
+
+        # loop through data to determine all signals
+        while end < 0:
+            curr_window = plot_df[start:end]
+            action = self.determine_signal(curr_window)[0]
+            plot_df.loc[plot_df.index[end - 1], 'signal'] = action
+            additional_info = self.determine_signal(curr_window)[1]
+            plot_df.loc[plot_df.index[end - 1], 'additional_info'] = additional_info
+            end += 1
+            start += 1
+
+        # compute final signal
+        plot_df.loc[plot_df.index[-1], 'signal'] = self.determine_signal(plot_df[-40:])[0]
+
+    def plot_graph(self):
+
+        # deep copy of data so original is not impacted
+        plot_df = self.df.copy(deep=True)
+
+        # determine all signals for the dataset
+        self.find_all_signals(plot_df)
+
+        plot_df['zero_line'] = 0
+        plot_df['25_line'] = 25
+
+        # initialise visualisation object for plotting
+        visualisation = v.Visualise(plot_df)
+
+        # determining one buy signal example for plotting
+        visualisation.determine_buy_marker()
+
+        # determining one sell signal example for plotting
+        visualisation.determine_sell_marker()
+
+        # add subplots of senkou span A and B to form the ichimoku cloud, and the parabolic sar dots
+        visualisation.add_subplot(plot_df['adx'], panel=1, color='blue', width=0.75, ylabel='ADX')
+        visualisation.add_subplot(plot_df['25_line'], panel=1, color='k', secondary_y=False, width=0.75, linestyle='solid')
+        visualisation.add_subplot(plot_df['+di'], panel=1, color='green', width=0.75)
+        visualisation.add_subplot(plot_df['-di'], panel=1, color='red', width=0.75)
+
+        # create final plot with title
+        visualisation.plot_graph("ADX Crossover Strategy")
