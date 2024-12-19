8c00a62590679f526b48609ca0a08fd5597bb50a
andre-goldmann <andre_goldmann@yahoo.de>
2024-12-19 19:59:22 +0100

    added other projects, updated eslint-config

---
+++ angular-projects/apps/forex-trades-analysis/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    if (navigator.storage && navigator.storage.persist) {
+      navigator.storage.persist().then((persistent) => {
+        if (persistent) {
+          console.log("Storage will not be cleared except by explicit user action");
+        } else {
+          console.log("Storage may be cleared by the UA under storage pressure.");
+        }
+      });
+    }
+  }
+  title = 'forex-trades-analysis';
+}
